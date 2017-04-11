de80926facdcfd2be6ed585dcb4eff4b0d47da6b
Bruno Paixao <brunomrpx@gmail.com>
2017-04-11 00:46:45 -0300

    Adjust in routes, router strategy and dependencies.

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 const routes: Routes = [
-  { path: 'customer', component: CustomerListComponent }
+  {
+    path: 'customer',
+    children: [
+      {
+        path: '',
+        component: SidebarComponent,
+        children: [
+          { path: '', component: CustomerListComponent }
+        ]
+      },
+    ]
+  }
 ];
 
 @NgModule({
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var componentHandler: any;
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  constructor() { }
+
+  ngAfterViewInit() {
+    componentHandler.upgradeAllRegistered();
+  }
+}
