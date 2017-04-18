3140d21d33090ea6524af770e7c035a952b762c1
Bruno Mayer Paixão <brunomrpx@gmail.com>
2017-04-18 08:43:21 -0300

    Criação dinâmica do menu.

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SidebarService } from '../sidebar/sidebar.service';
+
+export interface MenuItem {
+  label: string;
+  icon: string;
+  action: string | Function;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.less']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  private menuItems: MenuItem[] = [
+    { label: 'Lista de ECs', icon: 'fa-list', action: '/customer/list' }
+  ];
 
-  constructor() { }
+  constructor(private router: Router, private sidebarSerice: SidebarService) {}
 
-  ngOnInit() {
+  private goToAction(menuItem: MenuItem) {
+    console.log('go to action: ', menuItem);
+
+    this.sidebarSerice.toggleMenuLeft();
+
+    if (typeof menuItem.action === 'string') {
+      this.router.navigateByUrl(menuItem.action);
+    } else {
+      menuItem.action();
+    }
   }
-
 }
