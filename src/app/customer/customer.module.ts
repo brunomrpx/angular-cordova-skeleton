94fb706bb2ed7092c755232fc44e879033f712f3
Bruno Mayer Paixão <brunomrpx@gmail.com>
2017-04-20 15:41:28 -0300

    Filters working.

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
@@ -8,14 +9,25 @@ import { CustomerDetailComponent } from './customer-detail/customer-detail.compo
 import { CustomerFilterComponent } from './customer-filter/customer-filter.component';
 import { CustomerListHeaderComponent } from './customer-list/customer-list-header/customer-list-header.component';
 import { CustomerService } from './customer.service';
+import { CustomerFilterService } from './customer-filter/customer-filter.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     CustomerRoutingModule,
     SharedModule
   ],
-  declarations: [CustomerListComponent, CustomerDetailComponent, CustomerFilterComponent, CustomerListHeaderComponent],
-  providers: [CustomerService]
+  declarations: [
+    CustomerListComponent,
+    CustomerDetailComponent,
+    CustomerFilterComponent,
+    CustomerListHeaderComponent
+  ],
+  providers: [
+    CustomerService,
+    CustomerFilterService
+  ]
 })
 export class CustomerModule { }
